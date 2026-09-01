1184e5d33e4493ddc077f29130e5a2b442358361
ankit kumar gupta <ankitgpt2345@gmail.com>
2026-09-01 09:18:19 +0530

    add .env for deployment

--- customer-management-frontend/src/services/api.js
+++ customer-management-frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 const api=axios.create({
     baseURL:
-    "http://localhost:8081/api",
-     headers: {
+    import.meta.env.VITE_API_BASE_URL,
+    headers : {
         "Content-Type":"application/json",
-     },
+    },
+    
 });
 
 export default api;
